ba47957b3a7f4ff9e649ebbd891efa61fd16ad7e
Mangesh Suryawanshi <almtrainingcybage@gmail.com>
2019-10-08 21:48:05 +0530

    Add application.properties file along with getters & setters for employee class.

--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -1,7 +1,53 @@
 package payroll;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
 	public Employee() {	}
+	
+	private @Id @GeneratedValue Long id;
+	
+	private String firstname;
+	
+	private String lastname;
+	
+	private String designation;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
 	
 }
